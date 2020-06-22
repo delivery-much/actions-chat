216640b864867c942578c3a434bf375a20ca3aa8
g1stavo <gustavocsalvador@icloud.com>
2020-06-22 17:24:11 -0300

    Args directly

--- src/chat.js
+++ src/chat.js
@@ -12,14 +12,13 @@ const send = async (url) => {
 
   switch (github.context.eventName) {
     case 'pull_request': {
-      const { repo } = github.context.repo
-      const title = github.context.payload.pull_request.title
-      const author = github.context.actor
-      const htmlUrl = github.context.payload.pull_request.html_url
-
-      const body = openedPullRequest(repo, title, author, htmlUrl)
+      const body = openedPullRequest(
+        github.context.repo,
+        github.context.payload.pull_request.title,
+        github.context.actor,
+        github.context.payload.pull_request.html_url
+      )
       const response = await axiosInstance.post(url, body)
-
       if (response.status !== 200) throw new Error(`Google Chat notification failed. response status=${response.status}`)
       break
     }
