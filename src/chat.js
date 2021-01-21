aabae1d63fd763929bd7228bc08ac593f5465187
g1stavo <gustavocsalvador@gmail.com>
2021-01-21 10:53:57 -0300

    Ignore draft PRs (#2)
    * refactor: destructuring assignments
    
    * refactor: instance axios inside post function
    
    * refactor: move post to axios file
    
    * refactor: split handlers
    
    * feat: don't send message on draft pr
    
    * fix: add payload before acessing pr field

--- src/chat.js
+++ src/chat.js
@@ -1,34 +1,20 @@
 const github = require('@actions/github')
-const { newAxios } = require('./axios')
+const { post } = require('./axios')
 const { newPullRequest, newRelease } = require('./messages')
 
 /**
- * Send Google Chat message.
+ * Sends Google Chat message.
  *
  * @param {string} url - Google Chat Webhook URL
  */
 const send = async (url) => {
-  const axiosInstance = newAxios(url)
-
   switch (github.context.eventName) {
     case 'pull_request': {
-      const { repo } = github.context.repo
-      const title = github.context.payload.pull_request.title
-      const author = github.context.actor
-      const htmlUrl = github.context.payload.pull_request.html_url
-
-      const body = newPullRequest(repo, title, author, htmlUrl)
-      await post(axiosInstance, url, body)
+      await handlePullRequest(url)
       break
     }
     case 'release': {
-      const { repo } = github.context.repo
-      const tag = github.context.payload.release.tag_name
-      const author = github.context.actor
-      const htmlUrl = github.context.payload.release.html_url
-
-      const body = newRelease(repo, tag, author, htmlUrl)
-      await post(axiosInstance, url, body)
+      await handleRelease(url)
       break
     }
     default:
@@ -36,19 +22,26 @@ const send = async (url) => {
   }
 }
 
-/**
- * Do a HTTP POST with Axios.
- *
- * @param {AxiosInstance} axiosInstance - Axios instance
- * @param {string} url - POST URL
- * @param {object} body - POST body
- */
-const post = async (axiosInstance, url, body) => {
-  try {
-    await axiosInstance.post(url, body)
-  } catch (error) {
-    throw new Error(`Google Chat notification failed. ${error}}`)
+const handlePullRequest = async (url) => {
+  if (!github.context.payload.pull_request.draft) {
+    const { repo } = github.context.repo
+    const { title } = github.context.payload.pull_request
+    const { actor: author } = github.context
+    const { html_url: htmlUrl } = github.context.payload.pull_request
+
+    const body = newPullRequest(repo, title, author, htmlUrl)
+    await post(url, body)
   }
+}
+
+const handleRelease = async (url) => {
+  const { repo } = github.context.repo
+  const { tag_name: tag } = github.context.payload.release
+  const { actor: author } = github.context
+  const { html_url: htmlUrl } = github.context.payload.release
+
+  const body = newRelease(repo, tag, author, htmlUrl)
+  await post(url, body)
 }
 
 module.exports = { send }
